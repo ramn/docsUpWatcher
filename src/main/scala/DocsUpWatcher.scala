5ec8dec2c59d59b73b776c37ba77be355f2c3a10
Simon Andersson <simon.andersson@keybroker.com>
2012-11-14 11:07:54 +0100

    Initial commit
    Watches rss feed of http://docs-up.com for interesting changes

---
+++ src/main/scala/DocsUpWatcher.scala
@@ -0,0 +1,25 @@
+import scala.xml.{XML, Elem}
+
+object DocsUpWatcher extends App {
+
+  val watchedLinkPrefixes = io.Source.fromFile("watchedLinkPrefixes.txt").getLines.toList
+
+  val xmlData: Elem = XML.loadFile("RSSFeed.xml")
+
+  val linkRegex = """<a href="(http://docs-up.com/diff.*?)">.*?</a>""".r
+
+  val links = for {
+    contentElem <- (xmlData \\ "content")
+    val content = contentElem.text
+    linkMatch <- linkRegex.findAllIn(content).matchData
+    href <- linkMatch.subgroups
+    if isWatched(href)
+  } yield href
+
+  def isWatched(wrappedLink: String): Boolean =
+    watchedLinkPrefixes exists { prefix =>
+      wrappedLink contains prefix
+  }
+
+  links foreach println
+}
